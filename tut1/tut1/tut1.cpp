1b3182aaeaba262a6211934ea86588668fd6c1dd
Masienyane12 <prince.masienyane@gmail.com>
2016-02-24 15:55:54 +0200

    inputing a number and displaying

--- tut1/tut1/tut1.cpp
+++ tut1/tut1/tut1.cpp
@@ -4,10 +4,16 @@ using namespace std;
 int main()
 {
 	int num1;
-	cout << "hello world! " << endl;
+	cout << "hello user! please enter a number between 5 and 15 " << endl;
 	cin >> num1;
 
+	cout << "the number you entered is :" << num1 << endl;
 
+
+
+	
+
+	system("pause");
 	return 0;
 }
 
